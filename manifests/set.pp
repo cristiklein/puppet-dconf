5d39392bd1800d3d9de03d627ed6e05e555b2815
Cristian Klein <cristiklein@gmail.com>
2016-01-14 19:39:34 +0100

    Use existing session bus, if available

--- manifests/set.pp
+++ manifests/set.pp
@@ -11,5 +11,17 @@ define dconf::set ($value,$user,$group) {
     require   => Package['dconf-tools'],
     user      => $user,
     group     => $group,
+    onlyif    => 'test -z "$DBUS_SESSION_BUS_ADDRESS"',
+  }
+
+  exec { "dconf set $name inside user session":
+    command   => shellquote('dconf', 'write', $name, sorted_json($value)),
+    path      => "/usr/bin:/bin",
+    logoutput => "true",
+    provider  => 'shell',
+    require   => Package['dconf-tools'],
+    user      => $user,
+    group     => $group,
+    onlyif    => 'test -n "$DBUS_SESSION_BUS_ADDRESS"',
   }
 }
