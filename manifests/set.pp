8032d744e5d075793552b0b2d8d125563d3fa602
Cristian Klein <cristiklein@gmail.com>
2016-01-14 19:38:23 +0100

    More robust shell quoting

--- manifests/set.pp
+++ manifests/set.pp
@@ -1,22 +1,15 @@
-# It is a bit messy because of the use of eval but the idea is there:
-# * `dbus-launch --auto-syntax` sets some environment vars
-# * `dconf write` writes the conf
-#
-# Note: `value` needs to be escaped in a non-intuitive way. See the examples in the README.
-
 define dconf::set ($value,$user,$group) {
 	include dconf
 
 	debug "Dconf: set $name to $value for user $user and group $group"
 
-	exec { "dconf set $name":
-		command => "/bin/sh -c 'eval `dbus-launch --auto-syntax`'\" && dconf write $name \\\"$value\\\"\"",
-		path => "/usr/bin",
-		onlyif => "/bin/sh -c 'eval `dbus-launch --auto-syntax`'\" && test \\\"$value\\\" != \\\"`dconf read $name`\\\"\"",
-		logoutput => "true",
-		require => Package['dconf-tools'],
-		user => $user,
-		group => $group,
-		environment => "XDG_RUNTIME_DIR=/run/user/$user",
-	}
+  exec { "dconf set $name outside user session":
+    command   => shellquote('dbus-launch', '--exit-with-session', 'dconf', 'write', $name, sorted_json($value)),
+    path      => "/usr/bin",
+    logoutput => "true",
+    provider  => 'shell',
+    require   => Package['dconf-tools'],
+    user      => $user,
+    group     => $group,
+  }
 }
